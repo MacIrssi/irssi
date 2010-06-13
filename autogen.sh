6cb35f9b93b783e7c540b81c83de4d3a37a6f03a
Matt Wright <matt@sysctl.co.uk>
2010-06-13 15:55:44 -0700

    Stop erroring out when lynx doesn't exist

--- autogen.sh
+++ autogen.sh
@@ -42,7 +42,7 @@ elif type elinks >/dev/null 2>&1 ; then
   elinks -dump docs/faq.html|perl -pe 's/^ *//; if ($_ eq "\n" && $state eq "Q") { $_ = ""; } elsif (/^([QA]):/) { $state = $1 } elsif ($_ ne "\n") { $_ = "   $_"; };' > docs/faq.txt
 else
   echo "**Error**: No lynx or links present"
-  exit 1
+  #exit 1
 fi
 
 if test x$NOCONFIGURE = x && test -z "$*"; then
