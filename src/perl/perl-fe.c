d1a1c88207a57131944ea5319680e2f17b83c419
Matt Wright <matt@sysctl.co.uk>
2011-01-19 23:47:21 -0800

    Hurp. This gets called on unload, regardless of if perl_core is loaded. ><

--- src/perl/perl-fe.c
+++ src/perl/perl-fe.c
@@ -275,6 +275,4 @@ void fe_perl_deinit(void)
         signal_remove("script error", (SIGNAL_FUNC) sig_script_error);
 	signal_remove("complete command script load", (SIGNAL_FUNC) sig_complete_load);
 	signal_remove("complete command script unload", (SIGNAL_FUNC) sig_complete_unload);
-
-	perl_core_print_script_error(TRUE);
 }
